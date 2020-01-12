5869cea196f8b6597c59977772217cf3927dec5c
Igor Rocha <igortice@gmail.com>
2020-01-12 18:55:23 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+<p align="center">
+  <h3 align="center">React Native Template 1</h3>
+</p>
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import {Text, View} from 'react-native';
+
+import React from 'react';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Base Template 1</Text>
+    </View>
+  );
+};
+
+export default App;
